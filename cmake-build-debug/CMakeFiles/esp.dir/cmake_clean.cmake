4bea50d8653378e8139b53220e9c8522799ab2c7
Mustafa <m.mahmoud@gmx.at>
2016-12-07 16:03:41 +0100

    [MM] init

---
+++ cmake-build-debug/CMakeFiles/esp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esp.dir/assa.c.o"
+  "esp.pdb"
+  "esp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/esp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
